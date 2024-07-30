891b9f1c820f4acede807dc201d02d39c0216036
joseruperto01 <163691574+joseruperto01@users.noreply.github.com>
2024-07-30 12:09:25 -0300

    Add files via upload

---
+++ jaascript/main.js
@@ -0,0 +1,28 @@
+//alert("a ingresado a venta retro, mantener discrecion")
+
+function animateText(textArea) {
+    let text = textArea.value;
+    let to = text.length,
+      from = 0;
+
+    animate({
+      duration: 5000,
+      timing: bounce,
+      draw: function(progress) {
+        let result = (to - from) * progress + from;
+        textArea.value = text.slice(0, Math.ceil(result))
+      }
+    });
+  }
+
+
+  function bounce(timeFraction) {
+    for (let a = 0, b = 1; 1; a += b, b /= 2) {
+      if (timeFraction >= (7 - 4 * a) / 11) {
+        return -Math.pow((11 - 6 * a - 11 * timeFraction) / 4, 2) + Math.pow(b, 2)
+      }
+    }
+  }
+
+
+ 
