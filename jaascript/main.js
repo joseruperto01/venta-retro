a914f3725f4f4b5ea64d88b2039e10b14656facc
joseruperto01 <163691574+joseruperto01@users.noreply.github.com>
2024-08-07 12:08:58 -0300

    Update main.js

--- jaascript/main.js
+++ jaascript/main.js
@@ -1,5 +1,3 @@
-//alert("a ingresado a venta retro, mantener discrecion")
-
 function animateText(textArea) {
     let text = textArea.value;
     let to = text.length,
@@ -25,4 +23,35 @@ function animateText(textArea) {
   }
 
 
- 
+  var Slider = document.querySelector(".slider");
+  var IconoDerecho = document.querySelector(".icono-derecho");
+  var IconoIzuierdo = document.querySelector(".icono-izquierdo");
+  var Imagenes = document.querySelectorAll(".img-slider").length;
+  Contador = 0;
+  
+  function MoverSlider()
+    {
+      if(Contador > Imagenes - 1)
+          {
+            Contador = 0;
+          } else if(Contador < 0)
+                     {
+                      Contador =  Imagenes - 1;
+                     }
+      Slider.style.transition = "all 1s ease"
+      Slider.style.marginLeft = `-${100 * Contador}%`;
+    }
+  
+  function MoverDerecha()
+    {
+       Contador++;
+       MoverSlider();
+    }
+  IconoDerecho.addEventListener("click", MoverDerecha);
+  
+  function MoverIzquierda()
+    {
+      Contador--;
+      MoverSlider();
+    }
+  IconoIzuierdo.addEventListener("click", MoverIzquierda)
