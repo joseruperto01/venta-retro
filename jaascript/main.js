a1181f3e4cc2a56291bc7ac99d26927acef1a484
joseruperto01 <163691574+joseruperto01@users.noreply.github.com>
2024-11-14 21:05:12 -0300

    Update main.js

--- jaascript/main.js
+++ jaascript/main.js
@@ -1,3 +1,12 @@
+document.getElementById("B-compra").addEventListener("click", function() {
+	alert("ya has realizado la compra");
+})
+
+document.getElementById("postal").addEventListener("click", function() {
+	alert("ubicacion calculada");
+})
+
+
 function animateText(textArea) {
     let text = textArea.value;
     let to = text.length,
@@ -11,47 +20,47 @@ function animateText(textArea) {
         textArea.value = text.slice(0, Math.ceil(result))
       }
     });
-  }
+}
 
 
-  function bounce(timeFraction) {
+function bounce(timeFraction) {
     for (let a = 0, b = 1; 1; a += b, b /= 2) {
       if (timeFraction >= (7 - 4 * a) / 11) {
         return -Math.pow((11 - 6 * a - 11 * timeFraction) / 4, 2) + Math.pow(b, 2)
       }
     }
-  }
+}
 
 
-  var Slider = document.querySelector(".slider");
-  var IconoDerecho = document.querySelector(".icono-derecho");
-  var IconoIzuierdo = document.querySelector(".icono-izquierdo");
-  var Imagenes = document.querySelectorAll(".img-slider").length;
-  Contador = 0;
+var Slider = document.querySelector(".slider");
+var IconoDerecho = document.querySelector(".icono-derecho");
+var IconoIzuierdo = document.querySelector(".icono-izquierdo");
+var Imagenes = document.querySelectorAll(".img-slider").length;
+Contador = 0;
   
-  function MoverSlider()
-    {
-      if(Contador > Imagenes - 1)
-          {
-            Contador = 0;
-          } else if(Contador < 0)
-                     {
-                      Contador =  Imagenes - 1;
-                     }
-      Slider.style.transition = "all 1s ease"
-      Slider.style.marginLeft = `-${100 * Contador}%`;
-    }
+function MoverSlider()
+{
+    if(Contador > Imagenes - 1)
+        {
+          Contador = 0;
+        } else if(Contador < 0)
+                   {
+                    Contador =  Imagenes - 1;
+                   }
+    Slider.style.transition = "all 1s ease"
+    Slider.style.marginLeft = `-${100 * Contador}%`;
+}
   
-  function MoverDerecha()
+function MoverDerecha()
     {
        Contador++;
        MoverSlider();
     }
-  IconoDerecho.addEventListener("click", MoverDerecha);
+IconoDerecho.addEventListener("click", MoverDerecha);
   
-  function MoverIzquierda()
+function MoverIzquierda()
     {
       Contador--;
       MoverSlider();
     }
-  IconoIzuierdo.addEventListener("click", MoverIzquierda)
+IconoIzuierdo.addEventListener("click", MoverIzquierda)
